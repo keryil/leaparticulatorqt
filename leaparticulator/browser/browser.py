1f8216eeaff70b6dc2e3bec74b00d40bebe61731
Kerem Eryilmaz <keryilmaz@gmail.com>
2015-06-24 16:34:42 +0200

    Halfway through implementing HMM plotting for the browser. (reverted from commit fe86b4400bb0db67b1dc52e88a97cad13ee8b612) (reverted from commit a6be38c20e6a9661c0e29d41e34b697456d4e734)

--- leaparticulator/browser/browser.py
+++ leaparticulator/browser/browser.py
@@ -2,6 +2,7 @@ __author__ = 'kerem'
 
 import sys
 import sip
+
 try:
     sip.setapi('QDate', 2)
     sip.setapi('QDateTime', 2)
@@ -16,6 +17,7 @@ from PyQt4 import QtGui
 from PyQt4.QtCore import Qt
 
 import matplotlib
+
 matplotlib.use("Qt4Agg")
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
@@ -25,6 +27,7 @@ from leaparticulator.data.functions import fromFile
 import matplotlib.pyplot as plt
 import os
 from itertools import product
+
 
 class BrowserWindow(object):
     def __init__(self, parent=None):
@@ -65,13 +68,13 @@ class BrowserWindow(object):
         self.chkReversed = QtGui.QCheckBox("Reversed?")
 
         self.btnPlotTrajectory = QtGui.QPushButton('Plot trajectory')
-        connect(self.btnPlotTrajectory, "clicked()", lambda: self.plot_trajectory())
+        connect(self.btnPlotTrajectory, "clicked()", self.plot_trajectory)
 
         self.btnPlotHmmAndTrajectory = QtGui.QPushButton('Plot trajectory with HMM')
-        connect(self.btnPlotTrajectory, "clicked()", lambda: self.plot_hmm_and_trajectory())
+        connect(self.btnPlotHmmAndTrajectory, "clicked()", self.plot_hmm_and_trajectory)
 
         self.btnPlotHmm = QtGui.QPushButton('Plot HMM')
-        connect(self.btnPlotTrajectory, "clicked()", lambda: self.plot_hmm())
+        connect(self.btnPlotHmm, "clicked()", self.plot_hmm)
 
     def setup_splitter(self):
         self.layout.addWidget(self.splitter)
@@ -104,16 +107,24 @@ class BrowserWindow(object):
 
         self.file_tree.setSortingEnabled(True)
 
+    def update_status(self, string):
+        self.statusbar.showMessage(str(string))
+        from PyQt4.QtCore import QCoreApplication
+        app = QCoreApplication.instance()
+        app.processEvents()
+
+
     def on_select_file(self, new, old):
         selected = new.indexes()[0]
         fname = self.dir_model.fileInfo(selected).absoluteFilePath()
         if fname.endswith("exp.log") and (fname not in self.data):
-            self.statusbar.showMessage("Reading in log file %s" % fname)
+            self.update_status("Reading in log file %s" % fname)
             self.data[fname] = fromFile(fname)
+            self.display_file_data(self.data[fname])
         else:
-            self.statusbar.showMessage("Reloading log file %s" % fname)
-        self.display_file_data(self.data[fname])
+            return
         self.current_fname = fname
+        self.update_status(self.data[fname])
         self.statusbar.showMessage("Done")
 
     def display_file_data(self, data):
@@ -157,14 +168,13 @@ class BrowserWindow(object):
                        QtGui.QStandardItem(),
                        QtGui.QStandardItem()]
 
-                for v,r in zip(vals, row):
+                for v, r in zip(vals, row):
                     r.setData(rr[phase][meaning])
                     r.setText(v)
 
                 # row[-1].setData(rr[phase][meaning])
 
                 self.log_model.appendRow(row)
-
 
     def get_child(self, qtype, qname):
         obj = self.window.findChildren(qtype, qname)
@@ -175,14 +185,14 @@ class BrowserWindow(object):
 
     def plot_quiver2d(self, data, axis, alpha=.75, C=[], path=None, *args, **kwargs):
         X, Y = zip(*data[:-1])
-        u = [x1-x0 for x0,x1 in zip(X[:-1],X[1:])]
-        v = [y1-y0 for y0,y1 in zip(Y[:-1],Y[1:])]
+        u = [x1 - x0 for x0, x1 in zip(X[:-1], X[1:])]
+        v = [y1 - y0 for y0, y1 in zip(Y[:-1], Y[1:])]
         if not C:
             color_delta = 1. / (len(X) - 1)
-            C = [(color_delta*i,color_delta*i,color_delta*i) for i in range(len(X)-1)]
+            C = [(color_delta * i, color_delta * i, color_delta * i) for i in range(len(X) - 1)]
         X = X[:-1]
         Y = Y[:-1]
-        print map(len, [X,Y,u,v])
+        print map(len, [X, Y, u, v])
         patches = axis.quiver(X, Y, u, v, C, scale_units='xy', angles='xy', scale=1, width=0.005, alpha=alpha, **kwargs)
         return patches
 
@@ -237,13 +247,67 @@ class BrowserWindow(object):
         self.canvas.draw()
 
     def plot_hmm_and_trajectory(self):
-        pass
+        indexes = self.log_selection.selectedRows()
+        print map(lambda x: x.text(), map(self.log_model.itemFromIndex, indexes))
 
     def plot_hmm(self):
-        pass
+        from leaparticulator.notebooks.StreamlinedDataAnalysisGhmm import plot_hmm, unpickle_results
+        indexes = self.log_selection.selectedRows()
+        phase = self.log_model.itemFromIndex(indexes[0]).text()
+
+        # which phase are we interested in?
+
+        # first, ask the user which HMM they want to plot
+        import fnmatch
+        fname_to_logid = lambda x: os.path.split(x)[-1].split(".exp.log")[0]
+        print self.current_fname, fname_to_logid(self.current_fname)
+        matches = []
+        base_id = fname_to_logid(self.current_fname)
+        query = '%s*phase%d*.hmms' % (base_id, int(phase))
+        print "Searching for filename format: %s" % query
+        self.update_status("Searching %s recursively for HMM files associated with this trajectory..." % self.dir)
+        for root, dirnames, filenames in os.walk(self.dir, followlinks=True):
+            for filename in fnmatch.filter(filenames, query):
+                matches.append(os.path.join(root, filename))
+        hmm_file = self.pick_hmm_file(matches)
+
+        # unpickle the HMMs
+        self.update_status("Unpickling the HMMs...")
+        results = unpickle_results(hmm_file)
+        hmm = results.hmms[0]
+        for hmm_ in results.hmms:
+            if hmm.bic > hmm.bic:
+                hmm = hmm_
+
+        # now for the actual plotting
+        # create an axis
+        ax = self.figure.add_subplot(111)
+
+        # discards the old graph
+        ax.hold(False)
+
+        plot_hmm(hmm.means, hmm.transmat, hmm.variances, hmm.initProb, axes=ax)
+
+    def pick_hmm_file(self, filenames):
+        dialog = loadUiWidget("HmmFilePickerDialog.ui")
+        lst = dialog.findChildren(QtGui.QListWidget, "listWidget")[0]
+        lst.addItems(filenames)
+        dialog.exec_()
+        return lst.currentItem().text()
 
     def show(self):
         self.window.show()
+
+# class PickHMMDialog(QtGui.QDialog):
+#     def __init__(self, filelist, parent=None):
+#         super(PickHMMDialog, self).__init__(parent)
+#         self.list = QtGui.QListWidget(self)
+#         self.list.addItems(filelist)
+#         self.buttons = QtGui.QDialogButtonBox(
+#             QtGui.QDialogButtonBox.Ok | QtGui.QDialogButtonBox.Cancel,
+#             Qt.Horizontal, self)
+#         self.buttons.accepted.connect(self.accept)
+#         self.buttons.rejected.connect(self.reject)
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
