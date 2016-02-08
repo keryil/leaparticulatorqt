84ca870ee8c26bc8ca4999ffb45470dcde5eb288
Kerem Eryilmaz <keryilmaz@gmail.com>
2016-02-08 19:48:19 +0100

    Simplified P2P log reader tests.

--- leaparticulator/test/data/test_p2p_logs.py
+++ leaparticulator/test/data/test_p2p_logs.py
@@ -3,32 +3,21 @@ from leaparticulator.data.functions import fromFile_p2p, fromFile
 
 class TestP2PLogsFromTest(object):
     def setup_method(self, method):
-        self.test_file = './leaparticulator/test/test_data/P2P-160204.121143.1.exp.log'
+        if method in (self.test_detectP2PData,
+                      self.test_readRealData):
+            self.test_file = './leaparticulator/test/test_data/P2P-160203.170804.REALDATA.1.exp.log'
+        else:
+            self.test_file = './leaparticulator/test/test_data/P2P-160204.121143.1.exp.log'
+        print "Data file to use in the following test: {}".format(self.test_file)
 
     def teardown_method(self, method):
         pass
 
-    def test_read(self):
-        print fromFile_p2p(self.test_file)
+    def test_readTestData(self):
+        assert fromFile_p2p(self.test_file)
 
+    def test_readRealData(self):
+        assert fromFile_p2p(self.test_file)
 
-class TestP2PLogsFromReal(object):
-    def setup_method(self, method):
-        self.test_file = './leaparticulator/test/test_data/P2P-160203.170804.REALDATA.1.exp.log'
-
-    def teardown_method(self, method):
-        pass
-
-    def test_read(self):
-        print fromFile_p2p(self.test_file)
-
-
-class TestP2PLogsDetectP2P(object):
-    def setup_method(self, method):
-        self.test_file = './leaparticulator/test/test_data/P2P-160203.170804.REALDATA.1.exp.log'
-
-    def teardown_method(self, method):
-        pass
-
-    def test_read(self):
+    def test_detectP2PData(self):
         assert str(fromFile_p2p(self.test_file)) == str(fromFile(self.test_file))
